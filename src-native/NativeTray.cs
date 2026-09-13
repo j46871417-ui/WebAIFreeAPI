947abbc04a1fa2a9b3b3fa3db647da9ef4e3310f
j46871417-ui <j46871417-ui@users.noreply.github.com>
2026-09-13 11:55:47 +0500

    release: v1.9.0

--- src-native/NativeTray.cs
+++ src-native/NativeTray.cs
@@ -51,7 +51,7 @@ namespace WebAIFreeAPI.Native
         private void InitializeTray()
         {
             notifyIcon = new NotifyIcon();
-            notifyIcon.Text = "WebAIFreeAPI v1.8.3";
+            notifyIcon.Text = "WebAIFreeAPI v1.9.0";
 
             try
             {
